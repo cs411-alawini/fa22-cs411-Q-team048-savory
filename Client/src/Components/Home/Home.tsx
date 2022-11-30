25dab68beb0ef533cc0d41aa3b5a39fa7fecb133
yeshwanthsripathy <ys58@illinois.edu>
2022-11-30 09:56:15 -0600

    redirect when user is not authenticated

--- Client/src/Components/Home/Home.tsx
+++ Client/src/Components/Home/Home.tsx
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+import { authSelector } from "../Auth/AuthSlice";
 import Dashboard from "../Dashboard/Dashboard";
 import Navigation from "../Navigation/Navigation";
 import QueryEditor from "../QueryEditor/QueryEditor";
@@ -7,12 +10,20 @@ import Question from "../Question/Question";
 export default function Home() {
     const [navState, setNavState] = useState("0");
     const [navQuestionId, setNavQuestionId]=useState(0);
+    const authDetails = useSelector(authSelector);
     return (
         <div className="home">
-            <Navigation navState={navState} setNavState={setNavState}/>
+            {(authDetails.loginResult===undefined || authDetails.loginResult===false) && 
+            <div>
+                <Navigate to="/" replace/>
+            </div>}
+            {authDetails.loginResult===true && 
+            <div>
+                <Navigation navState={navState} setNavState={setNavState}/>
             {navState=='0' && <Dashboard />}
             {navState=='1' && <Question navState={navState} setNavState={setNavState} navQuestionId={navQuestionId} setNavQuestionId={setNavQuestionId}/>}
             {navState=='-1' && <QueryEditor questionId={navQuestionId}/>}
+            </div>}
         </div>
     );
 }
